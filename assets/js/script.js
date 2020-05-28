252aca2c9b03f896d9e6240e34268df43a4e021b
FAATAUIRA Heifara <faatauira.barry@gmail.com>
2020-05-28 12:59:52 -1000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function changeScreen()
+{
+    $("#screen1").addClass('d-none');
+    $("#screen2").removeClass('d-none');
+
+}
+
+function returnMain()
+{
+    $("#screen2").addClass('d-none');
+    $("#screen1").removeClass('d-none');
+
+}
